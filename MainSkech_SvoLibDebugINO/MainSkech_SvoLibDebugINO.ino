e7053bf160be00220eada9654c8742c252e57cff
nablam <lamribenn@gmail.com>
2021-01-04 23:52:56 -0500

    inited , kept standard git ignore in its place

---
+++ MainSkech_SvoLibDebugINO/MainSkech_SvoLibDebugINO.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MainSkech_SvoLibDebugINO.ino
+ Created:	1/4/2021 11:48:30 PM
+ Author:	lamri
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
