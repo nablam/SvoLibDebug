3cf7e1795f4a150a2cff707fb5f4dec924785d77
nablam <lamribenn@gmail.com>
2021-01-05 00:14:39 -0500

    git ignore updated

--- MainSkech_SvoLibDebugINO/MainSkech_SvoLibDebugINO.ino
+++ MainSkech_SvoLibDebugINO/MainSkech_SvoLibDebugINO.ino
@@ -6,10 +6,10 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(115200);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	Serial.println("hihoo");
 }
